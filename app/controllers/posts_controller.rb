f469751938f17a680797d45e46b1c31cc6a1c76c
tsugioueno <fmi4bm@icloud.com>
2020-09-14 14:28:59 +0900

    既読機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,9 +6,21 @@ class PostsController < ApplicationController
   # 削除
   # def new
   # end
-  
+
     def create
       Post.create(content: params[:content])
       redirect_to action: :index
+    end
+
+    def checked
+      post = Post.find(params[:id])
+      if post.checked 
+        post.update(checked: false)
+      else
+        post.update(checked: true)
+      end
+  
+      item = Post.find(params[:id])
+      render json: { post: item }
     end
 end
